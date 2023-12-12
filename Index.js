3d4d010f977d68bb428dc96a6ad407c75af16bbc
aman000712 <dbchhetri@gmail.com>
2023-12-12 20:58:57 +0545

    WEBSITE

---
+++ Index.js
@@ -0,0 +1,10 @@
+function shows(){
+    let data=document.getElementById('shows');
+
+    if(data.style.width==='100%'){
+    data.style.width='0%'
+
+    }else{
+    data.style.width='100%'
+    }
+}
